03a661a7c09b2dd7c36db7248ee9f2bb97217938
Ramon Pratar <ramon.prata@dtidigital.com.br>
2025-08-21 14:45:14 -0300

    fix package json

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -1,17 +1,17 @@
-import { storyblokInit, apiPlugin, getStoryblokApi } from "@storyblok/react";
+// import { storyblokInit, apiPlugin, getStoryblokApi } from "@storyblok/react";
 
-let _init = false;
+// let _init = false;
 
-export function initStoryblok() {
-  if (_init) return;
-  storyblokInit({
-    accessToken: process.env.STORYBLOK_TOKEN,
-    use: [apiPlugin],
-  });
-  _init = true;
-}
+// export function initStoryblok() {
+//   if (_init) return;
+//   storyblokInit({
+//     accessToken: process.env.STORYBLOK_TOKEN,
+//     use: [apiPlugin],
+//   });
+//   _init = true;
+// }
 
-export function storyblokApi() {
-  initStoryblok();
-  return getStoryblokApi();
-}
+// export function storyblokApi() {
+//   initStoryblok();
+//   return getStoryblokApi();
+// }
